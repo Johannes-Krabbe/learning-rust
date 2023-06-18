52803f451425e9e096315c8e4f38f38cbfbe80ca
Johannes Krabbe <Johannes@Krabbe.net>
2023-06-18 09:57:19 +0200

    add 'The Book'

---
+++ 002-hello_cargo/target/debug/deps/hello_cargo-61a49627c3fac201.d
@@ -0,0 +1,5 @@
+/Users/johanneskrabbe/Learning/learning-rust/002-hello_cargo/target/debug/deps/hello_cargo-61a49627c3fac201.rmeta: src/main.rs
+
+/Users/johanneskrabbe/Learning/learning-rust/002-hello_cargo/target/debug/deps/hello_cargo-61a49627c3fac201.d: src/main.rs
+
+src/main.rs:
